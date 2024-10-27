8f62f2e3ef1e54970ac02e44eca926d4bfa49061
Luisa Larrieu <larrieuestudos@gmail.com>
2024-10-27 11:35:01 -0300

    Tratamento de exceção - findById

--- src/main/java/com/larrieu/course/services/UserService.java
+++ src/main/java/com/larrieu/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.larrieu.course.entities.User;
 import com.larrieu.course.repositories.UserRepository;
+import com.larrieu.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -20,7 +21,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
